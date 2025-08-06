e9e71449f50a91a5eee92966270a09ebe677e623
fdrgsp <federico.gasparoli@gmail.com>
2025-08-06 12:58:54 -0400

    feat: initial setup

---
+++ src/qpm/__main__.py
@@ -0,0 +1,29 @@
+from PyQt6.QtWidgets import QApplication
+from qpm._qpm import QPMWidget
+import sys
+import traceback
+from types import TracebackType
+
+
+def _our_excepthook(
+    type: type[BaseException], value: BaseException, tb: TracebackType | None
+) -> None:
+    """Excepthook that prints the traceback to the console.
+
+    By default, Qt's excepthook raises sys.exit(), which is not what we want.
+    """
+    # this could be elaborated to do all kinds of things...
+    traceback.print_exception(type, value, tb)
+
+
+def main() -> None:
+    """Main function to run the QPM widget."""
+    app = QApplication(sys.argv)
+    win = QPMWidget()
+    win.show()
+    sys.excepthook = _our_excepthook
+    app.exec()
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/qpm/_qpm.py
@@ -0,0 +1,8 @@
+from PyQt6.QtWidgets import QWidget
+
+
+
+class QPMWidget(QWidget):
+    """The QPM widget."""
+    def __init__(self, parent: QWidget | None = None):
+        super().__init__(parent)
